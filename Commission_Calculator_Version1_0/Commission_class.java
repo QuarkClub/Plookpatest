859fb2e73965910e61bd512d3819c1fe745eec3d
yasaequark <yasaequark@gmail.com>
2016-11-15 18:33:05 +0700

    ทำการแตก Branch จาก Project "Commission_CalculatorVersion1.0" โดยทำการแก้ไข Method Commission_calculation ที่อยู่ใน Class ที่มีชื่อว่า Commission_class ซึ่ง แก้ไขให้ตรงกับความต้องการของลูกค้าที่ขอเปลี่ยนจาก Req.4.2.2

--- Commission_Calculator_Version1_0/Commission_class.java
+++ Commission_Calculator_Version1_0/Commission_class.java
@@ -14,11 +14,11 @@ public class Commission_class {
 	}
 	
 	public  void Commission_calculation(){
-		if(this.sales_figure >=1 && this.sales_figure <= 25000){
+		if(this.sales_figure >=1 && this.sales_figure <= 50000){
 			this.commission = 0.01;
-		}else if(this.sales_figure <=50000){
+		}else if(this.sales_figure <=100000){
 			this.commission = 0.02;
-		}else if(this.sales_figure > 50000){
+		}else if(this.sales_figure > 100001){
 			this.commission = 0.03;
 		}
 		this.compensation = this.salary +(this.sales_figure * this.commission);
