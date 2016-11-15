65de7a18253d9ed9fd2018200d02ccf6ffe4a13b
PCNOZOMI\Nozomi.f <faii.rujipat@gmail.com>
2016-11-15 17:40:19 +0700

    แก้ไขไฟล์ CommissionDemo - สร้าง Method Main ซึ่งเป็น Method ที่ใช้เรียก Method ใน Commission_class

--- Commission_Calculator/CommissionDemo.java
+++ Commission_Calculator/CommissionDemo.java
@@ -3,7 +3,13 @@ package com.Commission_Calculator;
 public class CommissionDemo {
 
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Plase input name : ");
+		String name = input.next();
+		System.out.print("Plase input salary : ");
+		double salary = input.nextDouble();
+		System.out.print("Plase input sales figure : ");
+		double sales_figure = input.nextDouble();
 	}
 
 }
