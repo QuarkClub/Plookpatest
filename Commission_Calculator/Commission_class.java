452eb9ee4d6935f44298c0c3f8effffff42fc9a5
Popsoften\poppykotic <popsoften@gmail.com>
2016-11-15 17:16:27 -0800

    แก้ไข class ที่มีชื่อว่า Commission_class ซึ่งทำการเพิ่มตัวแปรที่จำเป็นจะต้องใช้และเพิ่ม method constructor ซึ่งเป็น method ที่ใช้สำหรับรับค่าข้อมูล ชื่อพนักงาน เงินเดือนและยอดขายเพื่อที่จะตอบสนองต่อ Requirement ข้อที่ 4.2.1

--- Commission_Calculator/Commission_class.java
+++ Commission_Calculator/Commission_class.java
@@ -1,6 +1,17 @@
 package com.Commission_Calculator;
 
 public class Commission_class {
+	private String name="";	 	 //???????????
+	private double salary=0; 	 //?????????
+	private double sales_figure=0; 	 //??????
+	private double compensation=0;       //?????????
+	private double commission= 0;         //??????????
+	
+	public Commission_class(String name,double salary,double sales_figure) {
+		this.name = name;
+		this.salary = salary;
+		this.sales_figure = sales_figure;
+	}
 	
 
 }
